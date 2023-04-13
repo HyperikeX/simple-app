d60d025210f059ad977210a767cfbe9f7fb40172
HyperikeX <aj.rork@gmail.com>
2023-04-13 11:00:32 -0500

    added requirements.txt

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 def readDetails(filename):
     with open(filename, 'r') as f:
         return [line for line in f]
 
+
 @app.route("/user/<variable>")
 def greet(variable):
     return f"<p>Hello, {variable}</p>"
+
 
 @app.route("/")
 def homeInfo():
     name = "Anthony"
     content = ['This is my site', 'Hellow']
     content = readDetails('static/content.txt')
-    return render_template("base.html", name = name, aboutMe = content)
+    return render_template("base.html", name=name, aboutMe=content)
+
 
 @app.route("/form", methods= ['GET', 'POST'])
 def formDemo():
@@ -24,7 +28,7 @@ def formDemo():
     if request.method == 'POST':
         name = request.form('name')
 
-    return render_template('form.html', name = name)
+    return render_template('form.html', name=name)
 
 
-app.run(debug=True)
+app.run(debug=False)
