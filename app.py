68cb34bac90a8cb47ffb461c8aacd2074fd547b1
HyperikeX <aj.rork@gmail.com>
2023-04-13 10:05:34 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+# test
+
+app = Flask(__name__)
+
+def readDetails(filename):
+    with open(filename, 'r') as f:
+        return [line for line in f]
+
+@app.route("/user/<variable>")
+def greet(variable):
+    return f"<p>Hello, {variable}</p>"
+
+@app.route("/")
+def homeInfo():
+    name = "Anthony"
+    content = ['This is my site', 'Hellow']
+    content = readDetails('static/content.txt')
+    return render_template("base.html", name = name, aboutMe = content)
+
+@app.route("/form", methods= ['GET', 'POST'])
+def formDemo():
+    name = None
+    if request.method == 'POST':
+        name = request.form('name')
+
+    return render_template('form.html', name = name)
+
+
+app.run(debug=True)
